c34fe2b6955700db1448cffe7226c9da6385abd2
helmyFauzi22 <helmifauzi.siswa@smkn4bdg.sch.id>
2020-09-14 19:04:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/technovelty/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.technovelty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.Window;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private int loading=3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent Splash = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(Splash);
+                finish();
+            }
+        },loading);
+    }
+}
